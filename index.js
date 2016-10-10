84f49d753ed6845ae9234a22593e96645e0ae496
csf30816 <elijahporter@zoho.com>
2016-10-10 11:04:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -335,6 +335,19 @@ function settingsDialog() {
     swal("Oh no!", "This feature isn't available yet!", "error");
 }
 
-window.onbeforeunload = function(){
-    swal("Oh no!", "Are you sure you want to leave?", "question");
+var confirmOnPageExit = function (e) {
+    // If we haven't been passed the event get the window.event
+    e = e || window.event;
+
+    var message = 'Any text will block the navigation and display a prompt';
+
+    // For IE6-8 and Firefox prior to version 4
+    if (e) 
+    {
+        e.returnValue = message;
+    }
+
+    // For Chrome, Safari, IE8+ and Opera 12+
+    return message;
 };
+window.onbeforeunload = confirmOnPageExit;
